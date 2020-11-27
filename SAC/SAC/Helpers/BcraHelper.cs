5eeaff18f841fdac626e30ed1ae2bae33e6ab3bd
leonardo <time.dev.all@gmail.com>
2020-11-27 16:57:39 -0300

    inicio

---
+++ SAC/SAC/Helpers/BcraHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SAC.Helpers
+{
+
+    public static class BcraHelper
+    {
+        public static float GetCotizacion()
+        {
+            return 0;
+            //IEnumerable<StudentViewModel> students = null;
+
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri("http://localhost:64189/api/");
+            //    //HTTP GET
+            //    var responseTask = client.GetAsync("student");
+            //    responseTask.Wait();
+
+            //    var result = responseTask.Result;
+            //    if (result.IsSuccessStatusCode)
+            //    {
+            //        var readTask = result.Content.ReadAsAsync<IList<StudentViewModel>>();
+            //        readTask.Wait();
+
+            //        students = readTask.Result;
+            //    }
+            //    else //web api sent error response 
+            //    {
+            //        //log response status here..
+
+            //        students = Enumerable.Empty<StudentViewModel>();
+
+            //        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+            //    }
+            //}
+        }
+
+
+
+        public static System.Drawing.Image Base64ToImage(string base64String)
+        {
+            // Convert base 64 string to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            // Convert byte[] to Image
+            var ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            System.Drawing.Image imagen = System.Drawing.Image.FromStream(ms, true);
+            return imagen;
+        }
+    }
+
+}
---
+++ SAC/SAC/Models/CompraFacturaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAC.Models
+{
+    public class CompraFacturaViewModel
+    {
+
+
+    }
+}
